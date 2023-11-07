7ad4f0accb9c12e906b198b6d47a16ef22a1480e
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-11-07 23:26:13 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/example/jecoffee/JeCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jecoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeCoffeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeCoffeeApplication.class, args);
+    }
+
+}
