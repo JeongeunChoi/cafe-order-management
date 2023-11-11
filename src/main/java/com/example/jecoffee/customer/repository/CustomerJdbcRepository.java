5340e71bb6ecdcc851a817ca0d2937abcd6725ea
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-11-11 23:44:22 +0900

    feat: CustomerJdbcRepository 구현

---
+++ src/main/java/com/example/jecoffee/customer/repository/CustomerJdbcRepository.java
@@ -0,0 +1,58 @@
+package com.example.jecoffee.customer.repository;
+
+import com.example.jecoffee.customer.entity.Customer;
+import com.example.jecoffee.customer.entity.Email;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+import static com.example.jecoffee.global.JdbcUtils.toUUID;
+
+@Repository
+public class CustomerJdbcRepository implements CustomerRepository {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+
+    public CustomerJdbcRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    @Override
+    public Customer insert(Customer customer) {
+        int update = jdbcTemplate.update("INSERT INTO customers(customer_id, email)" +
+                " VALUES(UUID_TO_BIN(:customerId), :email)", toParamMap(customer));
+        if (update != 1) {
+        }
+        return customer;
+    }
+
+    @Override
+    public Optional<Customer> findByEmail(Email email) {
+        List<Customer> customers = jdbcTemplate.query(
+                "SELECT * FROM customers WHERE email = :email",
+                Collections.singletonMap("email", email.getAddress()),
+                customerRowMapper
+        );
+
+        return customers.isEmpty() ? Optional.empty() : Optional.of(customers.get(0));
+    }
+
+    private static Map<String, Object> toParamMap(Customer customer) {
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("customerId", customer.getCustomerId().toString().getBytes());
+        paramMap.put("email", customer.getEmail().getAddress());
+
+        return paramMap;
+    }
+
+    private static final RowMapper<Customer> customerRowMapper = (resultSet, i) -> {
+        UUID customerId = toUUID(resultSet.getBytes("customer_id"));
+        String email = resultSet.getString("email");
+
+        return Customer.createWithCustomerIdAndEmail(customerId, new Email(email));
+    };
+}
