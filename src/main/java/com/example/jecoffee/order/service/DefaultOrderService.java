f0e3b5a602d2a20c7ec3611f74ba6aa0aced62dd
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-11-12 00:11:23 +0900

    feat: 주문 취소 예외 처리 추가
    - 조리중이거나 배달완료된 경우 주문 취소가 불가하도록 예외 처리
    - 이미 취소된 주문인 경우 예외 처리

--- src/main/java/com/example/jecoffee/order/service/DefaultOrderService.java
+++ src/main/java/com/example/jecoffee/order/service/DefaultOrderService.java
@@ -1,6 +1,10 @@
 package com.example.jecoffee.order.service;
 
 import com.example.jecoffee.customer.entity.Email;
+import com.example.jecoffee.global.exception.DataException;
+import com.example.jecoffee.global.exception.DataExceptionCode;
+import com.example.jecoffee.order.exception.OrderException;
+import com.example.jecoffee.order.exception.OrderExceptionCode;
 import com.example.jecoffee.order.entity.Order;
 import com.example.jecoffee.order.entity.OrderItem;
 import com.example.jecoffee.order.entity.OrderStatus;
@@ -19,6 +23,7 @@ public class DefaultOrderService implements OrderService {
 
     private final OrderRepository orderRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(DefaultOrderService.class);
 
     public DefaultOrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
@@ -55,6 +60,14 @@ public class DefaultOrderService implements OrderService {
                 o -> {
                     if (o.getOrderStatus() == OrderStatus.BEFORE_ACCEPTANCE) {
                         orderRepository.updateOrderStatus(orderId, OrderStatus.CANCELED);
+                    } else if (o.getOrderStatus() == OrderStatus.CANCELED) {
+                        logger.error(OrderExceptionCode.CANNOT_BE_CANCELED.getMessage());
+                        throw new OrderException(OrderExceptionCode.ALREADY_CANCELED);
+                    } else {
+                        logger.error(OrderExceptionCode.CANNOT_BE_CANCELED.getMessage());
+                        throw new OrderException(OrderExceptionCode.CANNOT_BE_CANCELED);
+                    }
+                },
                 () -> {
                     logger.error(DataExceptionCode.DATA_NOT_EXIST.getMessage());
                     throw new DataException(DataExceptionCode.DATA_NOT_EXIST);
