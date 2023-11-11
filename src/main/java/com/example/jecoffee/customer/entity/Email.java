ad18dc29a4df840896542d5ca716f67615dca848
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-11-11 23:53:38 +0900

    feat: 이메일 점검 로직 및 예외 처리 추가
    - 이메일 길이 및 형식 점검
    - 이메일 중복 예외 처리

--- src/main/java/com/example/jecoffee/customer/entity/Email.java
+++ src/main/java/com/example/jecoffee/customer/entity/Email.java
@@ -11,9 +11,12 @@ public class Email {
 
     private String address;
 
+    private static final Logger logger = LoggerFactory.getLogger(Email.class);
 
     public Email(String address) {
+        if (checkAddressLength(address) && checkAddressFormat(address)) {
             this.address = address;
+        }
     }
 
     @Override
@@ -27,4 +30,21 @@ public class Email {
         return address;
     }
 
+    private boolean checkAddressLength(String address) {
+        if (address.length() >= 4 && address.length() <= 50) {
+            return true;
+        } else {
+            logger.error(CustomerExceptionCode.INVALID_EMAIL_LENGTH.getMessage());
+            throw new CustomerException(CustomerExceptionCode.INVALID_EMAIL_LENGTH);
+        }
+    }
+
+    private boolean checkAddressFormat(String address) {
+        if (Pattern.matches("\\b[\\w\\.-]+@[\\w\\.-]+\\.\\w{2,4}\\b", address)) {
+            return true;
+        } else {
+            logger.error(CustomerExceptionCode.INVALID_EMAIL_FORMAT.getMessage());
+            throw new CustomerException(CustomerExceptionCode.INVALID_EMAIL_FORMAT);
+        }
+    }
 }
--- src/main/java/com/example/jecoffee/customer/service/DefaultCustomerService.java
+++ src/main/java/com/example/jecoffee/customer/service/DefaultCustomerService.java
@@ -3,6 +3,10 @@ package com.example.jecoffee.customer.service;
 import com.example.jecoffee.customer.entity.Customer;
 import com.example.jecoffee.customer.entity.Email;
 import com.example.jecoffee.customer.repository.CustomerRepository;
+import com.example.jecoffee.customer.exception.CustomerException;
+import com.example.jecoffee.customer.exception.CustomerExceptionCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +14,7 @@ public class DefaultCustomerService implements CustomerService {
 
     private final CustomerRepository customerRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(DefaultCustomerService.class);
 
     public DefaultCustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
@@ -20,6 +25,8 @@ public class DefaultCustomerService implements CustomerService {
         if (!checkDuplicatedEmail(email)) {
             return customerRepository.insert(Customer.createWithEmail(email));
         }
+        logger.error(CustomerExceptionCode.DUPLICATED_EMAIL.getMessage());
+        throw new CustomerException(CustomerExceptionCode.DUPLICATED_EMAIL);
     }
 
     private boolean checkDuplicatedEmail(Email email) {
